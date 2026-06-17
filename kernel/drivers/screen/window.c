e0047e48a168a109a5db34d8f9aaa8f27522ca54
Kirill <larcenkokirill91@gmail.com>
2026-06-17 20:36:16 +0300

    3.16.5 - 1.2 wm

--- kernel/drivers/screen/window.c
+++ kernel/drivers/screen/window.c
@@ -7,3 +7,17 @@ void draw_window(unsigned char* video_memory, struct window* win) {
 	draw_rect(video_memory, win->lt.x + 2, win->lt.y , win->width - 3, WIN_HH - 1, 0, 0, 0, 255);
 	draw_rect(video_memory, win->lt.x, win->lt.y + 2, win->width, WIN_HH - 3, 0, 0, 0, 255);
 	draw_rect(video_memory, win->lt.x, win->lt.y + WIN_HH, win->width, win->height - WIN_WW, 255, 255, 255, 255);}
+void draw_xor_frame(unsigned int* vram, int x, int y, int width, int height, int pitch_dw) {
+    for (int i = 0; i < width; i++) {
+        int idx_top = y * pitch_dw + (x + i);
+        int idx_bottom = (y + height - 1) * pitch_dw + (x + i);
+        vram[idx_top] ^= 0x00FFFFFF;
+        vram[idx_bottom] ^= 0x00FFFFFF;
+    }
+    for (int j = 1; j < height - 1; j++) {
+        int idx_left = (y + j) * pitch_dw + x;
+        int idx_right = (y + j) * pitch_dw + (x + width - 1);
+        vram[idx_left] ^= 0x00FFFFFF;
+        vram[idx_right] ^= 0x00FFFFFF;
+    }
+}
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -5,9 +5,7 @@
 
 #include <kernel.h>
 
-// 1. Идеально чистая привязка буфера к отметке 64 МБ
 unsigned int* back_buffer32 = (unsigned int*)0x04000000; 
-
 unsigned int mouse_bg_buffer[256];
 int alt_pressed = 0;
 int menu_open = 0;
@@ -23,28 +21,21 @@ int last_h = -1;
 int start_menu_open = 0;
 int dragged_window_idx = -1;
 int is_mouse_pressed = 0;
-
 unsigned char current_scancode;
 unsigned int screen_pitch = 5120;
-
-// 2. Выносим pitch_dw в глобальные переменные, чтобы draw_time её видела!
 int pitch_dw = 1280; 
-
 static inline void outl(unsigned short port, unsigned int val) {
     __asm__ __volatile__("outl %0, %1" : : "a"(val), "Nd"(port));
 }
-
 static inline unsigned int inl(unsigned short port) {
     unsigned int ret;
     __asm__ __volatile__("inl %1, %0" : "=a"(ret) : "Nd"(port));
     return ret;
 }
-
 void (*draw_digit[10])(unsigned char*, int, int, unsigned char, unsigned char, unsigned char) = {
     draw_zero, draw_one, draw_two, draw_three, draw_four,
     draw_five, draw_six, draw_seven, draw_eight, draw_nine
 };
-
 void draw_time(unsigned char* back_buffer, unsigned char* video_memory) {
     get_rtc_time(&h, &m, &s);
     if (m != last_m || h != last_h) {
@@ -64,7 +55,6 @@ void draw_time(unsigned char* back_buffer, unsigned char* video_memory) {
         draw_digit[fm](back_buffer, 1216, 1000, 0, 0, 0);
         draw_rect(back_buffer, 1232, 1000, 20, 20, 229, 236, 253, 255);
         draw_digit[lm](back_buffer, 1232, 1000, 0, 0, 0);
-        
         unsigned int* src32 = (unsigned int*)back_buffer;
         unsigned int* dst32 = (unsigned int*)video_memory;
         for (int y = 1000; y < 1020; y++) {
@@ -75,7 +65,6 @@ void draw_time(unsigned char* back_buffer, unsigned char* video_memory) {
         }
     }
 }
-
 void sys_shutdown(void) {
     outl(0xCF8, 0x8000F840);
     io_wait();
@@ -94,7 +83,6 @@ void sys_shutdown(void) {
     io_wait();
     __asm__ __volatile__("cli; hlt");
 }
-
 void render(unsigned char* video_memory, struct superblock* fs) {
     draw_rect(video_memory, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 69, 178, 253, 255);
     draw_rect(video_memory, 0, 990, SCREEN_WIDTH, 40, 229, 236, 253, 255);
@@ -105,19 +93,16 @@ void render(unsigned char* video_memory, struct superblock* fs) {
         draw_os(video_memory, 640, 512, 255, 0, 0);
     }
 }
-
 static inline unsigned long long rdmsr(unsigned int msr) {
     unsigned int low, high;
     __asm__ __volatile__("rdmsr" : "=a"(low), "=d"(high) : "c"(msr));
     return ((unsigned long long)high << 32) | low;
 }
-
 static inline void wrmsr(unsigned int msr, unsigned long long val) {
     unsigned int low = (unsigned int)val;
     unsigned int high = (unsigned int)(val >> 32);
     __asm__ __volatile__("wrmsr" : : "c"(msr), "a"(low), "d"(high));
 }
-
 void enable_write_combining(unsigned int vram_base) {
     unsigned long long base_val = (vram_base & 0xFFFFF000) | 0x01;
     unsigned long long mask_val = 0xF000000000000000ULL | 0x00000000FF800000ULL | 0x800;
@@ -130,8 +115,6 @@ void enable_write_combining(unsigned int vram_base) {
     wrmsr(0x2FF, def_type | 0x400);
     __asm__ __volatile__("sti");
 }
-
-// 3. Твоя сверхбыстрая функция копирования
 static inline void flip_buffers(void* dest, const void* src, int dwords) {
     __asm__ __volatile__ (
         "cld\n\t"
@@ -153,6 +136,8 @@ void kernel_main(void) {
     int old_win_x = 0;
     int old_win_y = 0;
     int is_dragging = 0;
+    int frame_x = 0;
+    int frame_y = 0;
     struct window win[3];
     win[0].lt.x = 450;  win[0].lt.y = 400;
     win[0].width = 450; win[0].height = 200;
@@ -204,27 +189,31 @@ void kernel_main(void) {
                     if (mouse_y > SCREEN_HEIGHT - 16) mouse_y = SCREEN_HEIGHT - 16;
                     unsigned char left_button = (mouse_packet[0] & 0x01);
                     if (left_button == 1) {
-                        // МОМЕНТ ЗАХВАТА: Кнопка только что нажата, окно еще не тащат
                         if (prev_left_button == 0 && dragged_window_idx == -1 && start_menu_open == 0) {
                             for (int i = 0; i < 3; i++) {
                                 if (win[i].is_visible && mouse_x >= win[i].lt.x && mouse_x <= (win[i].lt.x + win[i].width) && mouse_y >= win[i].lt.y && mouse_y <= (win[i].lt.y + WIN_HH)) {
                                     dragged_window_idx = i;
-                                    
-                                    // Запоминаем Самые Начальные Координаты Окна!
                                     old_win_x = win[i].lt.x;
                                     old_win_y = win[i].lt.y;
                                     is_dragging = 1; 
+                                    frame_x = win[i].lt.x;
+                                    frame_y = win[i].lt.y;
+                                    draw_xor_frame(video_memory32, frame_x, frame_y, win[i].width, win[i].height, pitch_dw);
                                     break;
                                 }
                             }
                         }
                         if (dragged_window_idx != -1 && (move_x != 0 || move_y != 0)) {
+                            draw_xor_frame(video_memory32, frame_x, frame_y, win[dragged_window_idx].width, win[dragged_window_idx].height, pitch_dw);
                             win[dragged_window_idx].lt.x += move_x;
                             win[dragged_window_idx].lt.y -= move_y;
+                            frame_x = win[dragged_window_idx].lt.x;
+                            frame_y = win[dragged_window_idx].lt.y;
+                            draw_xor_frame(video_memory32, frame_x, frame_y, win[dragged_window_idx].width, win[dragged_window_idx].height, pitch_dw);
                         }
-                    } 
-                    else {
+                    } else {
                         if (dragged_window_idx != -1 && is_dragging == 1) {
+                            draw_xor_frame(video_memory32, frame_x, frame_y, win[dragged_window_idx].width, win[dragged_window_idx].height, pitch_dw);
                             render((unsigned char*)back_buffer32, fs);
                             for (int i = 0; i < 3; i++) {
                                 if (win[i].is_visible) {
@@ -264,7 +253,11 @@ void kernel_main(void) {
                 draw_cursor((unsigned char*)video_memory32, mouse_x, mouse_y);
             }
             if (menu_open == 1) {
-                if (data == 28) {
+                int line_y = win[0].lt.y + 165;
+                int left_line_x = win[0].lt.x + 50;
+                int right_line_x = win[0].lt.x + 300;
+
+                if (data == 28) { // Клавиша Enter
                     if (selected_option == 1) {
                         sys_shutdown();
                     } else {
@@ -285,15 +278,15 @@ void kernel_main(void) {
                 } 
                 else if (data == 77) {
                     selected_option = 1;
-                    draw_rect((unsigned char*)back_buffer32, 500, 565, 100, 4, 255, 255, 255, 255);
-                    draw_rect((unsigned char*)back_buffer32, 750, 565, 100, 4, 0, 120, 212, 255);
+                    draw_rect((unsigned char*)back_buffer32, left_line_x, line_y, 100, 4, 255, 255, 255, 255);
+                    draw_rect((unsigned char*)back_buffer32, right_line_x, line_y, 100, 4, 0, 120, 212, 255);
                     swap_buffers();
                     draw_cursor((unsigned char*)video_memory32, mouse_x, mouse_y);
                 } 
                 else if (data == 75) {
                     selected_option = 0;
-                    draw_rect((unsigned char*)back_buffer32, 750, 565, 100, 4, 255, 255, 255, 255);
-                    draw_rect((unsigned char*)back_buffer32, 500, 565, 100, 4, 0, 120, 212, 255);
+                    draw_rect((unsigned char*)back_buffer32, right_line_x, line_y, 100, 4, 255, 255, 255, 255);
+                    draw_rect((unsigned char*)back_buffer32, left_line_x, line_y, 100, 4, 0, 120, 212, 255);
                     swap_buffers();
                     draw_cursor((unsigned char*)video_memory32, mouse_x, mouse_y);
                 }
